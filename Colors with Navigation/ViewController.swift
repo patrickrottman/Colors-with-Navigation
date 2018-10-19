98b8d68694477b39c2df07da92c066991df631ed
Patrick Rottman <scimi2000@gmail.com>
2018-10-19 13:16:09 -0500

    Initial Commit

---
+++ Colors with Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors with Navigation
+//
+//  Created by Patrick Rottman on 10/19/18.
+//  Copyright © 2018 Patrick Rottman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
